93d561395b9efcaf7236f05b83e0356bfc23176c
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2021-02-13 14:09:19 +0100

    Restore Columns and Cards 'Create' funcionality

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,19 +2,24 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
-//import {settings} from '../../data/dataStore';
+import {settings} from '../../data/dataStore';
 
 class Column extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     cards: PropTypes.array,
     icon: PropTypes.string,
+    addCard: PropTypes.func,
+  }
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   }
 
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
 
     return (
       <section className={styles.component}>
@@ -23,18 +28,17 @@ class Column extends React.Component {
             <Icon name={icon} className={styles.icon}/>
           </span>
         </h3>
+
         <div className={styles.cards}>
           {cards.map(cardData => (
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
 
-        {/*
         <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+          <Creator text={settings.cardCreatorText} action={addCard}/>
         </div>
-        */}
-
+        
       </section> 
     );
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,7 +3,7 @@ import styles from './List.scss';
 import PropTypes from 'prop-types';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/ColumnContainer.js';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 
@@ -15,6 +15,7 @@ class List extends React.Component {
     image: PropTypes.string.isRequired,
     description: PropTypes.node,
     columns: PropTypes.array,
+    addColumn: PropTypes.func,
   }
 
   static defaultProps = {
@@ -22,26 +23,26 @@ class List extends React.Component {
   }
 
   render() {
-    const {title, image, description, columns} = this.props;
+    const {title, image, description, columns, addColumn} = this.props;
 
     return (
       <section className={styles.component}>
         <Hero titleText={title} titleImage={image} />
+        
         <div className={styles.description}>
           {ReactHtmlParser(description)}
         </div>
+  
         <div className={styles.columns}>
           {columns.map(columnData => (
             <Column key={columnData.id} {...columnData} />
           ))}
         </div>
 
-        {/*
         <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+          <Creator text={settings.columnCreatorText} action={addColumn}/>
         </div>
-        */}
-
+        
       </section>
     );
   }
